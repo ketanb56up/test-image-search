5181e19d3ebcab4aa2db2136cdd7847c8b00226f
ketan <ketanb56up@gmail.com>
2020-05-01 22:51:50 +0530

    added production build

---
+++ build/precache-manifest.7476008fd0dd9e43c975d625e91e1f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212da0f32c3484b345950072165b8c3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b782262fbd154301bf6f",
+    "url": "/static/css/main.2646dc89.chunk.css"
+  },
+  {
+    "revision": "cc8712592c3a69412aec",
+    "url": "/static/js/2.045d9781.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.045d9781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b782262fbd154301bf6f",
+    "url": "/static/js/main.29e81308.chunk.js"
+  },
+  {
+    "revision": "4b447b7fd50ea95888b8",
+    "url": "/static/js/runtime-main.3faaba8c.js"
+  }
+]);
